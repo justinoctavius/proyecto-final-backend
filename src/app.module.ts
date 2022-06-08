fbd8abadf9f90dc3688ac22d402bc5a9c6ac8a98
justinoctavius <justinoctavio2001@gmail.com>
2022-06-07 21:25:39 -0400

    feat: dockerizing proyect

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env['DATABASE_HOST'],
+      port: Number(process.env['DATABASE_PORT']),
+      username: process.env['DATABASE_USER'],
+      password: process.env['DATABASE_PASSWORD'],
+      database: process.env['DATABASE_NAME'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
