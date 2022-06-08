4b8057a029182252758ac1f68a12c658635cf45f
justinoctavius <justinoctavio2001@gmail.com>
2022-06-07 21:47:19 -0400

    feat: adding services bolierplate

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { CategoryController } from './controllers/category/category.controller';
+import { TransactionController } from './controllers/transaction/transaction.controller';
+import { CategoryService } from './services/category.service';
+import { TransactionService } from './services/transaction.service';
 
 @Module({
-  controllers: [CategoryController, TransactionModule],
-  providers: [],
+  controllers: [CategoryController, TransactionController],
+  providers: [TransactionService, CategoryService],
 })
 export class TransactionModule {}
